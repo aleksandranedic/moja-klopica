b6d8181cbf431bbde944864aa4121054b1213b63
Aleksandra Nedic <nedicaleksandra843@gmail.com>
2023-01-09 13:01:16 +0100

    Disable updating order

--- moja-klopica-back/src/order/order.service.ts
+++ moja-klopica-back/src/order/order.service.ts
@@ -8,7 +8,6 @@ import { RestaurantService } from 'src/restaurant/restaurant.service';
 import { Repository } from 'typeorm';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderItemsDto } from './dto/order-items.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { Order } from './entities/order.entity';
 import { OrderItem } from './entities/orderItem.entity';
 
@@ -68,10 +67,6 @@ export class OrderService {
 
   findOne(id: number) {
     return `This action returns a #${id} order`;
-  }
-
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
   }
 
   remove(id: number) {
